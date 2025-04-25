846f14fbdd61f8f5303f6fe4931e1be6f5e863e7
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-04-25 14:59:11 +0000

    modified

--- app/(customers)/page.tsx
+++ app/(customers)/page.tsx
@@ -2,31 +2,18 @@ import React from "react";
 import { Metadata } from "next";
 import Hero from "@/components/landing/HeroSection";
 import BaseIcon from "@/components/landing/Base-content";
-import ProductCard from "@/components/ui/custom-card";
 export const meta: Metadata = {
   title: "Customers",
   description: "Customers",
 };
+import HotDealsSection from "@/components/landing/HotDealsSection";
 
 const HomePage = () => {
   return (
     <main className=" mx-auto">
       <Hero />
       <BaseIcon />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {Array.from([1, 2, 3, 4]).map((item, idx) => (
-          <ProductCard
-            key={idx}
-            className="custom-class"
-            name="Sample Product"
-            price={29.99}
-            rating={4.5}
-            discount={10}
-            imageAlt="Sample Product Image"
-            isHotDeal={item === 1}
-          />
-        ))}
-      </div>
+      <HotDealsSection />
     </main>
   );
 };
--- components/landing/HotDealsSection.tsx
+++ components/landing/HotDealsSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductCard from "../ui/custom-card";
 import { PRODUCTS } from "@/constants/products";
+import ImageBlur from "../common/ImageBlur";
 
 const HOTDEAL = {
   imageUrl: "/images/product5.jpg",
@@ -15,7 +16,18 @@ const HOTDEAL = {
 const HotDealsSection: React.FC = () => {
   return (
     <section className="hot-deals-section">
-      <h2 className="section-title">Hot Deals</h2>
+      <h2 className="section-title flex justify-between">
+        <span className="block text-2xl font-bold">Hot Deals</span>
+        <span className="flex text-[#00B207] gap-4">
+          View ALL{" "}
+          <ImageBlur
+            src="/icons/more.png"
+            alt="more icon"
+            width={20}
+            height={20}
+          />
+        </span>
+      </h2>
       <div className="products-grid">
         {PRODUCTS.map((product, index) => (
           <ProductCard
